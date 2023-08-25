8a339807515c52ac8341b81bf275e9f5279ec885
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2023-08-25 14:51:12 +1200

    Add YUV 420 conversions

--- taichi_image/test/test_yuv.py
+++ taichi_image/test/test_yuv.py
@@ -3,19 +3,22 @@ import cv2
 import numpy as np
 import torch
 
-from taichi_image import color_conversion
+from taichi_image import color
 from taichi_image.test.arguments import init_with_args
+
+def show_rgb(img):
+  bgr = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  cv2.imshow('img', bgr)
+  cv2.waitKey(0)
 
 
 def show_yuv420(img):
   if isinstance(img, torch.Tensor):
     img = img.cpu().numpy()
 
-  bgr = cv2.cvtColor(img, cv2.COLOR_YUV2BGR_I420)
-
-  cv2.imshow('img', bgr)
-  cv2.waitKey(0)
-
+  # bgr = cv2.cvtColor(img, cv2.COLOR_YUV2BGR_I420)
+  rgb = color.yuv420_rgb_image(img)
+  show_rgb(rgb)
 
 def main():
     args = init_with_args()
@@ -23,12 +26,11 @@ def main():
     img = cv2.imread(args.image)
     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
-    yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV_I420)
-    yuv2 = color_conversion.rgb_yuv420_image(rgb)
-
+    # yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV_I420)
+    yuv2 = color.rgb_yuv420_image(rgb)
 
     if args.show:
-      show_yuv420(yuv)
+      show_rgb(rgb)
       show_yuv420(yuv2)
         
 
